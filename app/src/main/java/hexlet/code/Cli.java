bd1d770576af8ca2fe3c977e1a52891be7a1ce01
Covch <covch.it@yandex.ru>
2023-05-08 15:21:02 +0300

    checkstyle refactoring

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,9 +3,9 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-	public static void newUserGreetings() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("May I have your name? ");
-		System.out.println("Hello, " + sc.next() + "!");
-	}
+    public static void newUserGreetings() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        System.out.println("Hello, " + sc.next() + "!");
+    }
 }
